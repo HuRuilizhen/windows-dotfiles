9d55524943c777b64515921251e7373a28f4be2d
HuRuilizhen <huruilizhen@gmail.com>
2026-08-09 23:19:25 +0800

    fix(nvim): fix embedded webview issue using local browser

---
+++ nvim/lua/plugins/peek.lua
@@ -0,0 +1,11 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+---@type LazySpec
+return {
+  {
+    "toppair/peek.nvim",
+    opts = {
+      app = "browser",
+    },
+  },
+}
